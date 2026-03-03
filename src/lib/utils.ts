dff3fa600e8c15b24a0fd532a16088491041f29b
Sirimongkol Manubut <Topt75870@gmail.com>
2026-03-03 17:29:55 +0700

    Add more profanity words

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,7 +1,7 @@
 export const BAD_WORDS = [
   "ควย", "เย็ด", "เหี้ย", "สัส", "มึง", "กู", "ดอกทอง", "ระยำ", "จัญไร",
-  "เงี่ยน", "หี", "อมหำ", "เสียบ", "น้ำแตก", "เลีย", "ดูด", "ขย่ม", "ซอย",
-  "fuck", "shit", "bitch", "asshole", "dick", "pussy", "cock", "sex"
+  "เงี่ยน", "หี", "อมหำ", "เสียบ", "น้ำแตก", "เลีย", "ดูด", "ขย่ม", "ซอย", "คัน", "ตอก", "ฟิน",
+  "fuck", "shit", "bitch", "asshole", "dick", "pussy", "cock", "sex", "horny"
 ];
 
 export const SELF_HARM_KEYWORDS = [
